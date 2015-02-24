e56f4d4eac1505668f4a71c7c45b5d27f5cfd456
Panos Paganis <mallinanga@gmail.com>
2015-02-24 17:48:37 +0200

    Plugin Control class

---
+++ includes/class-nanga-plugin-control.php
@@ -0,0 +1,36 @@
+<?php
+
+class Nanga_Plugin_Control {
+    static $instance;
+    private $disabled = array();
+
+    public function __construct( Array $disables = null ) {
+        if ( is_array( $disables ) ) {
+            foreach ( $disables as $disable ) {
+                $this->disable( $disable );
+            }
+        }
+        add_filter( 'option_active_plugins', array(
+            $this,
+            'do_disabling'
+        ) );
+        self::$instance = $this;
+    }
+
+    public function disable( $file ) {
+        $this->disabled[] = $file;
+    }
+
+    public function do_disabling( $plugins ) {
+        if ( count( $this->disabled ) ) {
+            foreach ( (array) $this->disabled as $plugin ) {
+                $key = array_search( $plugin, $plugins );
+                if ( false !== $key ) {
+                    unset( $plugins[ $key ] );
+                }
+            }
+        }
+
+        return $plugins;
+    }
+}
