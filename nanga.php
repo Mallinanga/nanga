aae8751a7760d0d9adf78e388a6e3e496793d3c5
Panos Paganis <mallinanga@gmail.com>
2015-02-27 16:39:59 +0200

    1.0.1

--- nanga.php
+++ nanga.php
@@ -9,7 +9,7 @@
  * Plugin URI:        https://github.com/Mallinanga/nanga
  * GitHub Plugin URI: https://github.com/Mallinanga/nanga
  * Description:       Functions that don't belong to the theme.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Panos Paganis
  * Author URI:        https://github.com/Mallinanga
  * License:           GPL-2.0+
