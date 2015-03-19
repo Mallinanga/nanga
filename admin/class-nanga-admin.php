5e29f8b449ac2551902375bc4e220dfebf667d6b
Panos Paganis <mallinanga@gmail.com>
2015-03-20 00:54:11 +0200

    Some of there should be enabled

--- admin/class-nanga-admin.php
+++ admin/class-nanga-admin.php
@@ -254,9 +254,9 @@ class Nanga_Admin {
     }
 
     public function disable_widgets() {
-        unregister_widget( 'WP_Nav_Menu_Widget' );
+        //unregister_widget( 'WP_Nav_Menu_Widget' );
+        //unregister_widget( 'WP_Widget_Categories' );
         unregister_widget( 'WP_Widget_Calendar' );
-        unregister_widget( 'WP_Widget_Categories' );
         unregister_widget( 'WP_Widget_Links' );
         unregister_widget( 'WP_Widget_Meta' );
         unregister_widget( 'WP_Widget_Pages' );
