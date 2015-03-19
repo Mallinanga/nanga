fc7d78f4f21d8a6faa1ea3d80881be09f80a7c92
Panos Paganis <mallinanga@gmail.com>
2015-03-20 00:57:28 +0200

    Icon column should have a label

--- admin/class-nanga-admin.php
+++ admin/class-nanga-admin.php
@@ -522,7 +522,7 @@ class Nanga_Admin {
         //$post_types = get_post_types( array( 'public' => true ), 'names' );
         //if ( in_array( $post_type, $post_types, true ) ) { unset( $columns['cb'] ); }
         if ( post_type_supports( $post_type, 'thumbnail' ) && 'product' != $post_type ) {
-            $columns = array( 'icon' => false ) + $columns;
+            $columns = array( 'icon' => __( 'Featured', $this->nanga ) ) + $columns;
         }
 
         return $columns;
@@ -542,7 +542,7 @@ class Nanga_Admin {
 
     public function columns_pages( $columns ) {
         if ( post_type_supports( 'page', 'thumbnail' ) ) {
-            $columns = array( 'icon' => false ) + $columns;
+            $columns = array( 'icon' => __( 'Featured', $this->nanga ) ) + $columns;
         }
 
         return $columns;
