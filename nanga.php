759daf228ab38a9d07eec88d5409166299c19708
Panos Paganis <mallinanga@gmail.com>
2015-11-22 22:56:55 +0200

    Bump self to 1.1.7

--- nanga.php
+++ nanga.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://github.com/Mallinanga/nanga
  * GitHub Plugin URI: https://github.com/Mallinanga/nanga
  * Description:       Functions that don't belong to the theme.
- * Version:           1.1.6
+ * Version:           1.1.7
  * Author:            Panos Paganis
  * Author URI:        https://github.com/Mallinanga
  * Text Domain:       nanga
